0951972dd6b6c537631773256bf4c863ebcea172
danielvogler <davogler@ethz.ch>
2021-04-21 16:41:04 +0200

    compile pdf-files, remove unnecessary files

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # shuffled_exam
 Creates shuffled versions of exam from given exam questions
+
+### How-to
+- Place individual exercises in folder `/exercises/`
+- Set exam name, participant names etc. in `shuffled_exam`
+- If necessary, add packages to main tex file `master_exam`
+- generate individual tests for participants with
+`bash shuffled_exam` 
--- shuffled_exam.sh
+++ shuffled_exam.sh
@@ -1,6 +1,8 @@
 ### daniel vogler
 ### generate pdf with shuffled questions
 
+mkdir "./exams/"
+
 ### file in student names for individualized tests
 student_names=("Smith" "Miller" "Hightower" "Baxter" "Gorin")
 
@@ -8,7 +10,8 @@ student_names=("Smith" "Miller" "Hightower" "Baxter" "Gorin")
 base="master_exam"
 
 ### name of final test
-test_all="exam"
+exam_folder="./exams/"
+test_all="${exam_folder}exam"
 rm -f ${test_all}*
 
 ### exercise files to incorporate into main test
@@ -35,5 +38,15 @@ for s in ${student_names[@]}; do
 	done
 
 	echo "\end{document}" >> $test_student
+	
+	pdflatex -interaction batchmode -output-directory $exam_folder $test_student
 
 done
+
+### delte all files except pdf-files
+rm -f ${test_all}*log
+rm -f ${test_all}*aux
+rm -f ${test_all}*tex
+
+
+
